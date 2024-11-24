ef585872f4b6be7404afb016da9c3143209e4861
KalwiRemi <piterparker101@gmail.com>
2024-11-24 02:23:29 +0000

    initial setup

---
+++ fetch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+download () {
+  yt-dlp "$1" \
+    -j \
+    --no-download \
+    --lazy-playlist \
+    --break-on-reject \
+    --dateafter today-1month \
+      | jq -r '[.id, .channel_id, .channel_url, .webpage_url, .timestamp, .epoch, .upload_date, .duration, .duration_string, .view_count, .live_status, .is_live, .was_live, .title, .fulltitle, .like_count, .comment_count, .thumbnail, .channel_follower_count, .uploader, .uploader_id, .channel] | @csv' > data.csv
+}
+
+sqlite3 ./dist/database.sqlite < setup_db.sql
+
+while read -r url; do
+  download "$url"
+  ./import.py
+done < urls.txt
