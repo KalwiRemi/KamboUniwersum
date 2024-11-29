34bd10821e6f7ffd066c8e1aff399c8273b50518
KalwiRemi <piterparker101@gmail.com>
2024-11-29 23:11:52 +0000

    ba ba ba

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <GoogleTagManager gtmId="G-5KHWDKBZ81" />
+      <GoogleAnalytics gaId="G-5KHWDKBZ81" />
     </html>
   )
 }
