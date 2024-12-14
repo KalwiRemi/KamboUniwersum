4a850644f48b654da88018cb30129706d4da9fba
KalwiRemi <piterparker101@gmail.com>
2024-12-14 23:23:53 +0000

    susdej ciuran bababa

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import { unstable_cache, unstable_noStore } from 'next/cache';
 import { Video } from '@/actions/fetchYoutubeVideos'
 import { fetchAllChannelVideos } from '../utils/fetchYoutubeVideos';
 import VideoList from './components/VideoList';
 
-const getVideos = unstable_cache(
-  fetchAllChannelVideos,
-  ['videos'],
-  { revalidate: 300, tags: ['videos'] }
-);
+export const revalidate = 60;
 
 export default async function Home() {
-  unstable_noStore();
   try {
-    const videos: Video[] = await getVideos();
+    const videos: Video[] = await fetchAllChannelVideos();
 
     return (
       <main className="container mx-auto px-4 py-8">
